84bbfe8d961fde5945c2e19aaf8f85a961a547b6
Alistair Baco <alistair.baco@universite-paris-saclay.fr>
2025-03-24 09:38:00 +0100

    First Commit, basic game structure, no interactions

---
+++ ennemi.gd
@@ -0,0 +1,8 @@
+extends CharacterBody2D
+
+@onready var player = get_node("/root/Jeu/TomateMain")
+
+func _physics_process(delta):
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * 200
+	move_and_slide()
---
+++ jeu.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+const OBJET_ENNEMI = preload("res://ennemi.tscn")
+
+func _ready():
+	apparaitre_ennemi()
+
+func apparaitre_ennemi() : 
+	var nouvel_ennemi = OBJET_ENNEMI.instantiate()
+	%CheminSpawn.progress_ratio= randf()
+	nouvel_ennemi.global_position = %CheminSpawn.global_position
+	add_child(nouvel_ennemi)
+	
+
+
+func _on_timer_timeout():
+	apparaitre_ennemi()
