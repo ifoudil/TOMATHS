592602006a95d7b3d436af030a76cbb9852c27e5
Calliixte <alicee.baco@gmail.com>
2025-03-25 14:49:24 +0100

    Les ennemis meurent en touchant la tomate

--- Scripts/ennemi.gd
+++ Scripts/ennemi.gd
@@ -1,13 +1,11 @@
 extends CharacterBody2D
 
-@onready var player = get_node("/root/Jeu/TomateMain")
-
-signal eliminate()
-# queue free will kill the thing
-
+@onready var player = get_node("/root/Jeu/TomateMain") # il y a une meilleure maniere de le faire en l'assignant quand il spawn je crois
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 200
 	move_and_slide()
-	
+
+func die():
+	queue_free()
--- Scripts/tomate_main.gd
+++ Scripts/tomate_main.gd
@@ -1,8 +1,15 @@
 extends CharacterBody2D
 
-
+var vie = 10 #nombre de points de vie de la tomate
 
 func _physics_process(delta):
 	var liste_entites_touche = %Hurtbox.get_overlapping_bodies()
-		if liste_entites_touche.size()!=0:
-			#emit kill signal
+
+
+
+func _on_hurtbox_body_entered(body):
+	vie-=1
+	if(vie == 0):
+		queue_free()
+	if body.has_method("die"):
+		body.die() # Replace with function body.
