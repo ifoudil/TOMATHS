3fad9f020547aebc0431938022887ee63177a503
Calliixte <alicee.baco@gmail.com>
2025-03-25 17:47:05 +0100

    fix mineur a la barre de vie et gestion des ennemis

--- Scripts/tomate_main.gd
+++ Scripts/tomate_main.gd
@@ -9,11 +9,10 @@ func _physics_process(delta):
 
 
 func _on_hurtbox_body_entered(body):
-
-	if(vie == 0):
-		queue_free()
 	if body.has_method("die"):
 		vie-=1
+		if(vie == 0):
+				visible= false
 		%BarreDeVie.value=vie # ne change la valeur de la barre de vie que si l'objet qui rentre est un ennemi (sinon la barre de vie met des degats a la tomate)
 		body.die() # Replace with function body.
 		
