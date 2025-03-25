eba3547b81f1263a9feca1950a66eec2d097c337
Calliixte <alicee.baco@gmail.com>
2025-03-25 15:10:14 +0100

    Added health bar

--- Scripts/tomate_main.gd
+++ Scripts/tomate_main.gd
@@ -2,14 +2,18 @@ extends CharacterBody2D
 
 var vie = 10 #nombre de points de vie de la tomate
 
+
 func _physics_process(delta):
 	var liste_entites_touche = %Hurtbox.get_overlapping_bodies()
 
 
 
 func _on_hurtbox_body_entered(body):
-	vie-=1
+
 	if(vie == 0):
 		queue_free()
 	if body.has_method("die"):
+		vie-=1
+		%BarreDeVie.value=vie # ne change la valeur de la barre de vie que si l'objet qui rentre est un ennemi (sinon la barre de vie met des degats a la tomate)
 		body.die() # Replace with function body.
+		
