c2ccf1940ab6502c85fa9e795a9cb09d8cef274a
Calliixte <alicee.baco@gmail.com>
2025-04-05 16:42:06 +0200

    changements inputs, debut questions
    -mode manette
    -differents binds pour écrire des expressions
    -fonctionnement des questions par un dictionnaire qui associe la question à sa réponse, il faut ensuite charger ce dictionnaire depuis un fichier, pour faire des gros sets de questions on pourra donc generer des fichiers a partir d'un autre langage

--- Scripts/encadréréponse.gd
+++ Scripts/encadréréponse.gd
@@ -1,5 +1,50 @@
 extends LineEdit
 
+var nombreChange = false
+
+func _physics_process(delta):
+	var actions_texte = {
+		"puissance" : "^",
+		"a": "a",
+		"division": "/",
+		"inconnue 1 (x)": "x",
+		"moins": "-",
+		"plus": "+"
+	}
+
+	for action in actions_texte.keys():
+		if Input.is_action_just_pressed(action):
+			if nombreChange : 
+				text+= %PrevueNombre.text
+				nombreChange=false
+			text += actions_texte[action]
+
+	if Input.is_action_just_pressed("delete"):
+		resetText()
+		
+	if Input.is_action_just_pressed("confirmer (manette)"):
+		text+= %PrevueNombre.text
+		nombreChange=false
+		
+	for i in range(10):
+		if Input.is_action_just_pressed(str(i)):
+			text += str(i)
+
+
+	if Input.is_action_just_pressed("plusNombre (manette)"):
+		var nombre = int(%PrevueNombre.text) #meme s'il n'y a rien le cast en int va interpreter comme 0
+		if nombre <9 : 
+			nombre +=1
+		%PrevueNombre.text = str(nombre)
+		nombreChange = true
+	if Input.is_action_just_pressed("moinsNombre (manette)"):
+		var nombre = int(%PrevueNombre.text) #meme s'il n'y a rien le cast en int va interpreter comme 0
+		if nombre > 0 : 
+			nombre -=1
+		%PrevueNombre.text = str(nombre)
+		nombreChange = true
+	
+
 func getText():
 	return text
 
--- Scripts/ennemi.gd
+++ Scripts/ennemi.gd
@@ -2,16 +2,45 @@ extends CharacterBody2D
 
 @onready var player = get_node("/root/Jeu/TomateMain") # il y a une meilleure maniere de le faire en l'assignant quand il spawn je crois
 
+const VITESSE_ENNEMI = 50
+
+var questRep = {"x^a":"ax^a-1",
+"1/x":"-1/x^2",
+}
+
+var text_file_path = "res://Questions/derivees.txt" 
+
+func _ready() : 
+	var text_content = get_text_file_content(text_file_path) #en gros faut lire depuis le fichier pour remplir le dictionnaire (et donc vider celui qu'il y a la)
+	print(text_content)
+	
+
+func get_text_file_content(filePath):
+	var file = FileAccess.open(filePath, FileAccess.READ)
+	var content = file.get_as_text()
+	return content
+	
+
+
+
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * 200
+	velocity = direction * VITESSE_ENNEMI
 	move_and_slide()
-
+	
 func die():
 	queue_free()
 
+
 func setQuestion(): #on pourra lui passer la question en parametre apres
-	%Question.text = str(randi() % 10) #entre 0 et 9
+	for i in questRep: #pire manière possible de selectionner une valeur aléatoire
+		if randi() % 10 <=2:
+			%Question.text=i;
+			break;
+		else :
+			%Question.text=i;
+		
+
 	
 func getReponse():
-	return %Question.text
+	return questRep[%Question.text];
