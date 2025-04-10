15ea6a6e8bc9e8def573dd7aa79a4c211734f854
Islem <islem.foudil@universite-paris-saclay.fr>
2025-04-10 23:16:08 +0200

    ajout des sound effects pour main page

--- Scripts/tomaths_main_page.gd
+++ Scripts/tomaths_main_page.gd
@@ -5,15 +5,25 @@ extends Control
 @onready var start_btn = $StartBtn
 @onready var exit_btn = $ExitBtn
 
+@onready var start = $AudioStreamPlayer2D
+@onready var splash = $AudioStreamPlayer2D2
+
+
 func _ready():
 	start_btn.connect("button_down", Callable(self, "_on_start_btn_button_down"))
 	exit_btn.connect("button_down", Callable(self, "_on_exit_btn_button_down"))
 	animation_coulis.connect("animation_finished", Callable(self, "_on_animation_finished"))
+	start.play() 
+	start.connect("finished", Callable(self, "_on_music_finished"))  
+
+func _on_music_finished():
+	start.play()  
 
 func _on_start_btn_button_down() -> void:
 	print("Start button clicked")
 	animation_coulis.speed_scale = 1.2
 	animation_coulis.play("coulis")
+	splash.play()
 
 func _on_animation_finished(anim_name: String) -> void:
 	print("Animation finished:", anim_name)
